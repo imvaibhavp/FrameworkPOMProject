be929edf32d997f71f0811924f64a3f8687db1c5
Prakash <vaibhavprakash@mind.motherson.com>
2022-08-19 12:23:39 +0530

    edited deleted and modified

---
+++ src/main/java/com/qa/opencart/pages/DemoPage2.java
@@ -0,0 +1,6 @@
+package com.qa.opencart.pages;
+
+public class DemoPage2 {
+
+	int i=10;
+}
--- src/test/java/com/qa/opencart/tests/LoginTests.java
+++ src/test/java/com/qa/opencart/tests/LoginTests.java
@@ -14,30 +14,30 @@ public class LoginTests extends BaseTest {
 		Assert.assertEquals(acttitle, AppConstraints.LOGIN_PAGE_TITLE);
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 3)
 	public void getUrl() {
 		String actURL = loginPage.getCurrentUrl();
 		Assert.assertEquals(actURL,AppConstraints.LOGIN_PAGE_URL);
 	}
 
-	@Test(priority = 3)
+	@Test(priority = 4)
 	public void getUrl1() {
 		String actURL1 = loginPage.getCurrentUrl();
 		Assert.assertEquals(actURL1.contains(AppConstraints.LOGIN_PAGE_URL_FRACTION), true);
 	}
 
-	@Test(priority = 4)
+	@Test(priority = 5)
 	public void getLink() {
 		Assert.assertEquals(loginPage.getLinkPresent(), true);
 	}
 
-	@Test(priority = 5)
+	@Test(priority = 6)
 	public void doLoginTest() {
 		accPage = loginPage.doOpenCartLogin(prop.getProperty("Username"),prop.getProperty("Password"));
 		Assert.assertEquals(accPage.getAccountsPageTitle(), AppConstraints.Home_TITLE);
 	}
 	
-	@Test(priority = 5)
+	@Test(priority = 2)
 	public void doLoginTest1() {
 		accPage = loginPage.doOpenCartLogin(prop.getProperty("Username"),prop.getProperty("Password"));
 		Assert.assertEquals(accPage.getAccountsPageTitle(), AppConstraints.Home_TITLE);
